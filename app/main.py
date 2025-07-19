6f4eaea447af41bd36b0a96f713cd65df8ce3393
Savio Fernando <fsavio27@gmail.com>
2025-07-19 14:17:54 +0000

    Feat: Mongo Store Implemented

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
+from app.db.stores import MONGO_STORE
 from app.api import router as api_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    MONGO_STORE.connect()
     yield
+    MONGO_STORE.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
