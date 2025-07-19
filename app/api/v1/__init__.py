20f90b49db694e47224c70bf599770a33b09a431
Savio Fernando <fsavio27@gmail.com>
2025-07-19 14:19:09 +0000

    Fix: Added Health Router to V1 Router back

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from .health_check import router as health_router
 
-router = APIRouter("/v1")
-router.include_router(health_check)
+router = APIRouter(prefix="/v1")
+router.include_router(health_router)
